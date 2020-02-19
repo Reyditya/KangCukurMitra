c27cbaab399cfd381e6376a57b62a33c5b816013
Myquotes <dirhaam06@gmail.com>
2020-02-19 10:50:13 +0700

    push bottomnavigation

--- app/src/main/java/id/ionbit/kangcukur_mitra/View/Activity/MainActivity.java
+++ app/src/main/java/id/ionbit/kangcukur_mitra/View/Activity/MainActivity.java
@@ -1,10 +1,20 @@
 package id.ionbit.kangcukur_mitra.View.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import id.ionbit.kangcukur_mitra.Helper.BottomNavigationViewHelper;
 import id.ionbit.kangcukur_mitra.R;
+import id.ionbit.kangcukur_mitra.View.Fragment.HomeFragment;
+import id.ionbit.kangcukur_mitra.View.Fragment.InboxFragment;
+import id.ionbit.kangcukur_mitra.View.Fragment.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +22,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.inbox:
+                        fragment = new InboxFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fl_container, fragment);
+                transaction.commit();
+                return true;
+            }
+        });
+    }
+
+    private boolean loadFragment(HomeFragment homeFragment) {
+        if (homeFragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, homeFragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    protected void onResume() {
+        super.onResume();
     }
 }
