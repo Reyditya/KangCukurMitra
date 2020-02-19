35a15f906b70940fd99bb9b57e11ceb1c904f6af
Myquotes <dirhaam06@gmail.com>
2020-02-19 10:32:33 +0700

    Initial commit

---
+++ app/src/main/java/id/ionbit/kangcukur_mitra/Helper/BottomNavigationViewHelper.java
@@ -0,0 +1,38 @@
+package id.ionbit.kangcukur_mitra.Helper;
+
+import android.annotation.SuppressLint;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Reyditya on 29/12/2018.
+ */
+
+public class BottomNavigationViewHelper {
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+//                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+
+        } catch (IllegalAccessException e) {
+
+        }
+    }
+}
---
+++ app/src/main/java/id/ionbit/kangcukur_mitra/View/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ionbit.kangcukur_mitra.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ionbit.kangcukur_mitra.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
